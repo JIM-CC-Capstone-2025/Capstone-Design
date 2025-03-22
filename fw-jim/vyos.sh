92995b099e8231d5730854c44a211f17290857a1
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-03-22 12:22:43 -0400

    Update vyos.sh

--- fw-jim/vyos.sh
+++ fw-jim/vyos.sh
@@ -4,3 +4,6 @@ set interfaces ethernet eth1 address '192.168.1.1/24'
 set interfaces ethernet eth1 description 'LAN'
 set interfaces ethernet eth2 address '192.168.2.1/24'
 set interfaces ethernet eth2 description 'DMZ'
+set zone-policy zone WAN interface eth0
+set zone-policy zone LAN interface eth1
+set zone-policy zone DMZ interface eth2
