5fd41e319c2e0a2ad6a11a52e6423325d6021fc6
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-03-22 12:48:02 -0400

    Update vyos.sh

--- fw-jim/vyos.sh
+++ fw-jim/vyos.sh
@@ -1,4 +1,4 @@
-set interfaces ethernet eth0 address [figure out later]
+set interfaces ethernet eth0 address 10.0.17.200
 set interfaces ethernet eth0 description 'WAN'
 set interfaces ethernet eth1 address '192.168.1.1/24'
 set interfaces ethernet eth1 description 'LAN'
@@ -7,3 +7,10 @@ set interfaces ethernet eth2 description 'DMZ'
 set zone-policy zone WAN interface eth0
 set zone-policy zone LAN interface eth1
 set zone-policy zone DMZ interface eth2
+set protocols static route 0.0.0.0/0 next-hop 10.0.17.2
+set service dns forwarding allow-from 10.0.17.0/24
+set service dns forwarding listen-address 10.0.17.2
+set service dns forwarding system
+set system name-server 10.0.17.2
+
+
