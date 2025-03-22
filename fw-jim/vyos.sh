f3c3bd2b067f30fa7ab9094b99e09cb749ad625b
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-03-22 13:11:13 -0400

    Update vyos.sh

--- fw-jim/vyos.sh
+++ fw-jim/vyos.sh
@@ -8,11 +8,11 @@ set zone-policy zone WAN interface eth0
 set zone-policy zone LAN interface eth1
 set zone-policy zone DMZ interface eth2
 set protocols static route 0.0.0.0/0 next-hop 10.0.17.2
-set service dns forwarding allow-from 10.0.17.0/24
-set service dns forwarding listen-address 10.0.17.2
+set service dns forwarding allow-from 192.168.1.0/24
+set service dns forwarding allow-from 192.168.2.0/24
+set service dns forwarding listen-address 192.168.1.1
 set service dns forwarding system
 set system name-server 10.0.17.2
 set nat source rule 20 outbound-interface eth0
 set nat source rule 20 source address 192.168.1.1/24
 set nat source rule 20 translation address 'masquerade'
-
