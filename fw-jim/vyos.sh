e29ca7b5ed44adbe3e5b8d8de36c498d36a5785e
morganconnors <74949548+morganconnors@users.noreply.github.com>
2025-03-22 12:20:26 -0400

    Create vyos.sh

---
+++ fw-jim/vyos.sh
@@ -0,0 +1,6 @@
+set interfaces ethernet eth0 address NULL
+set interfaces ethernet eth0 description 'WAN'
+set interfaces ethernet eth1 address '192.168.1.1/24'
+set interfaces ethernet eth1 description 'LAN'
+set interfaces ethernet eth2 address '192.168.2.1/24'
+set interfaces ethernet eth2 description 'DMZ'
