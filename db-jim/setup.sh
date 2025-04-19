7bc0395e0c2b8b0955f1dbd18ce943582925c0e0
Isaiah River <122812369+Isaiah-River@users.noreply.github.com>
2025-04-18 22:49:01 -0400

    Update setup.sh

--- db-jim/setup.sh
+++ db-jim/setup.sh
@@ -3,6 +3,4 @@
 yum upgrade -y
 yum install git curl mysql neovim -y
 
-sudo yum install npm -y
-npm init -y
-npm install express cors body-parser bcryptjs mysql2 dotenv
+sudo yum install mysql-server -y
