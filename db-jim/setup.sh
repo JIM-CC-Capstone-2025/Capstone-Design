2d2ad137846e98a69106bece6ba56cfbdaf5143c
Morgan Connors <github@morganconnors.com>
2025-05-26 09:31:51 -0400

    update

--- db-jim/setup.sh
+++ db-jim/setup.sh
@@ -1,4 +1,7 @@
 #! bin/bash
 
 yum upgrade -y
-yum install git curl mysql neovim -y
+yum install git curl wget neovim -y
+sudo dnf install mysql-server
+sudo systemctl enable mysqld.service --now
+sudo mysql_secure_installation
